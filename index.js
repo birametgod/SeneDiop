0b73cdf50d69c47bc814aa00dc69d754b571d3f0
birametgod <birametgod95@gmail.com>
2018-06-30 10:36:45 +0000

    ts

--- index.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { YellowBox } from 'react-native';
-AppRegistry.registerComponent('VoteProject', () => App);
-YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
